b550b5ef1a625ec2f3a1d37dc83e2aba6106d6cb
Max <butrin.maksim@gmail.com>
2020-04-11 22:17:22 +0300

    the first commit with code

---
+++ src/Geekbrains/Geekbrains.cpp
@@ -0,0 +1,24 @@
+#include <Geekbrains.h>
+
+#include <QDebug>
+#include <QString>
+
+//#define HOMEWORK "Homework 02 task 01"
+
+using namespace std;
+
+void homework01Part01()
+{
+	//qDebug() << HOMEWORK;
+	
+	short varShort = 45000;
+	unsigned char varUnsignedChar = 1400;
+	
+	
+
+	//QString message = "Value of %1 if equal %2";
+
+	//qDebug() << message.arg("varShort").arg(varShort);
+	//qDebug() << message.arg("varUnsigned").arg(varUnsignedChar);
+
+}
---
+++ src/Geekbrains/main.cpp
@@ -0,0 +1,10 @@
+#include <qdebug.h>
+#include <Geekbrains.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	qDebug() << "tess";
+	homework01Part01();
+}
